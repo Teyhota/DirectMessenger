f229cb640f6d89ea7dd02fc5cada74704452bee2
Teyhota <boulwaretyler@gmail.com>
2016-12-15 21:04:56 -0800

    Add files via upload

--- DirectMessengerConfig.cs
+++ DirectMessengerConfig.cs
@@ -3,7 +3,6 @@ using System.Xml;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
 using System.ComponentModel;
